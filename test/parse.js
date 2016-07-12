658ba8516172506a98c129f4d539c43047c6ad24
Tom Davies <tvdavies@gmail.com>
2016-07-12 13:56:23 +0100

    Remove ES6 code from tests as well for backwards compatibility.

--- test/parse.js
+++ test/parse.js
@@ -3,14 +3,14 @@ var chai = require('chai'),
 
 var expect = chai.expect;
 
-describe('Parse Directives', () => {
+describe('Parse Directives', function () {
 
-	it('{title:Song Title} should set song.title property to \'Song Title\'', () => {
+	it('{title:Song Title} should set song.title property to \'Song Title\'', function () {
 		var song = chordprojs.parse('{title:Song Title}');
 		expect(song.title).to.equal('Song Title');
 	});
 
-	it('{subtitle:Song Subtitle} should set song.subtitle property to \'Song Subtitle\'', () => {
+	it('{subtitle:Song Subtitle} should set song.subtitle property to \'Song Subtitle\'', function () {
 		var song = chordprojs.parse('{subtitle:Song Subtitle}');
 		expect(song.subtitle).to.equal('Song Subtitle');
 	});
